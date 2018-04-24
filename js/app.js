5ad0c99d5f2df35af4c4b8a9d6321946b4578dbb
Lenox89 <cameron.macon@gmail.com>
2018-04-24 12:26:47 -0700

    Add project boilerplate

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module('GSApp', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('GSApp').config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+    });
+});
